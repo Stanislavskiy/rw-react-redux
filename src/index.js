5436fb44fe68b0a49c96979ed61dd336e3907c67
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-14 12:03:03 +0200

    Checkbox state & toggle action

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { createStore } from 'redux';
+
+/*-----------------------------
+  REDUX
+-------------------------------*/
+
+const defaultState = {
+  checked:false,
+}
+
+const reducer = (state, action) => {
+
+  switch (action.type) {
+    case 'TOGGLE': {
+      return {
+        ...state,
+        checked: !state.checked
+      }
+    }
+  }
+}
+
+const store = createStore(reducer, defaultState);
+
+/*-----------------------------
+  APP
+-------------------------------*/
 
 class App extends React.Component {
   render() {
