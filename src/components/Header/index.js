5c63b4ac571673eaae09e1595faba56b70e6bb8b
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-15 17:27:06 +0200

    Header component: add links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,21 @@ class Header extends React.Component {
     return (
       <nav className="navbar navbar-light">
         <div className="container">
-          <Link to="/" className="navbar-brand">{this.props.appName.toLowerCase()}</Link>
+          <Link to="/" className="navbar-brand">
+            {this.props.appName.toLowerCase()}
+          </Link>
+          <ul className="nav navbar-nav pull-xs-right">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="login" className="nav-link">
+                Sign in
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     );
