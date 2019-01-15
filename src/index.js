d0d8fcbcabc80cf887040ec375980d8be68dc485
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-15 14:55:54 +0200

    Import fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import { createStore } from "redux";
 import { Provider } from 'react-redux';
 import App from './components/App'; 
+import store from './store';
 
 ReactDOM.render(
   (
