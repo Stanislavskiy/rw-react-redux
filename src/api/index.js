08d12b8645954389122b61ee17b86a3609cc034e
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-16 10:55:03 +0200

    Api utils refactoring

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,15 @@
-import axios from "axios";
-
-const API_ROOT = "https://conduit.productionready.io/api";
-
-const urls = {
-  articles: `${API_ROOT}/articles?limit=10`
-};
+import axios from 'axios';
+import urls from 'endpoints';
 
 export function getArticles() {
-  return axios.get(urls.articles).then(response => {
-    return response.data.articles;
-  }).catch(error => {
-    console.error(error);
-    
-    return [];
-  });
-};
+  return axios
+    .get(urls.articles)
+    .then(response => {
+      return response.data.articles;
+    })
+    .catch(error => {
+      console.error(error);
+
+      return [];
+    });
+}
