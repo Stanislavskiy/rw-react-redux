3edfffbf9bbd051bf1c8978d1811640db88be50f
stanislavskiy <stanislavskiy.sanya@gmail.com>
2019-01-16 11:31:21 +0200

    Add API utils

---
+++ src/api/utils.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+
+export function endpointSet({ root_url = '', endpoints = {} }) {
+  // Combine endpoints with root url
+  const urls = _.mapValues(endpoints, val => {
+    return `${root_url}${val}`;
+  });
+
+  return urls
+}
